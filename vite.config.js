f961987c872d736572c42e9381953946bf88aa28
Arthur Apoyan <arthur.apoyan.96@gmail.com>
2024-02-19 16:01:23 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    build: {
+        outDir: 'dist', // Specify the output directory as 'dist'
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
         }),
-        laravel(['resources/js/app.tsx']),
         react(),
     ],
 });
